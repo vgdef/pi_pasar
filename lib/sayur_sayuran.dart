891d42729a2a20f64dff5037b4d5f437420461ba
vgdef <valensyagdef@icloud.com>
2021-07-23 15:56:54 +0900

    forth modify

--- lib/sayur_sayuran.dart
+++ lib/sayur_sayuran.dart
@@ -15,32 +15,45 @@ class SayurSayuran {
 var sayurSayuran = [
   SayurSayuran(
     name: 'Sereh',
-    price: 'Rp. 1000',
+    price: 'Rp. 1.000',
     description:
         'Sereh / Serai Fresh dan Segar ukuran 1000gram.Jenis sereh lokal BUKAN sereh bangkok',
-    imageAsset: 'images/sereh.jpg',
+    imageAsset: 'images/sereh.png',
   ),
   SayurSayuran(
       name: 'Wortel',
-      price: 'Rp. 2000',
+      price: 'Rp. 2.000',
       description:
           'Wortel Impor - HARGA PER Pcs +/- @80-250gr (berat dapat bervariasi)',
-      imageAsset: 'images/wortel.jpg'),
+      imageAsset: 'images/wortel.png'),
   SayurSayuran(
       name: 'Kangkung',
-      price: 'Rp. 7000',
+      price: 'Rp. 7.000',
       description: 'Kangkung Hijau Fresh Per / ikat',
-      imageAsset: 'images/kangkung.jpg'),
+      imageAsset: 'images/kangkung.png'),
   SayurSayuran(
       name: 'Paprika',
-      price: 'Rp. 5000',
+      price: 'Rp. 5.000',
       description:
           'Paprika Mix Colour / Bell Pepper Mix Colour. Paprika selalu menjadi sayuran populer untuk ditanam di kebun. Beri jarak bibit 18-24 inci terpisah dan di baris 24 hingga 36 inci terpisah.',
-      imageAsset: 'images/paprika.jpg'),
+      imageAsset: 'images/paprika.png'),
   SayurSayuran(
-      name: 'Pockcoy',
-      price: 'Rp. 6500',
-      description:
-          'Sayur Pokcoy/ pok coy/pok choy/bok coy dgn harga tertera adalah untuk 500 gr.',
-      imageAsset: 'images/pockcoy.jpg'),
+    name: 'Pockcoy',
+    price: 'Rp. 6.500',
+    description:
+        'Sayur Pokcoy/ pok coy/pok choy/bok coy dgn harga tertera adalah untuk 500 gr.',
+    imageAsset: 'images/pockcoy.png',
+  ),
+  SayurSayuran(
+    name: 'Tomat Ceri',
+    price: 'Rp. 5.000/Kg',
+    description: 'Tomat Ceri segar yang dipetik langsung dari kebun tetangga',
+    imageAsset: 'images/tomat.png',
+  ),
+  SayurSayuran(
+    name: 'Brokoli',
+    price: 'Rp. 12.000',
+    description: 'Brokoli',
+    imageAsset: 'images/brokoli.png',
+  ),
 ];
